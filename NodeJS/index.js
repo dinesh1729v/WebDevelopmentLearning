72a88b3174b638507c3075682660c692512d251e
Dinesh Vujuru <dinesh1729v@gmail.com>
2020-06-14 04:30:09 +0530

    Learnt basics of node.js and npm

---
+++ NodeJS/index.js
@@ -0,0 +1,6 @@
+var superheroes = require("superheroes");
+var mySuperHeroName = superheroes.random();
+console.log(mySuperHeroName);
+var supervillians = require("supervillains");
+var mySuperVillainName= supervillians.random();
+console.log(mySuperVillainName);
